b140fbd38f822fc3ab5779cd6dcc1a8b10305485
enilder <louisf.pierre@gmail.com>
2018-01-07 18:22:39 -0500

    Added percentiles.py script

---
+++ percentiles.py
@@ -0,0 +1,16 @@
+# percentile scoring
+
+def percentileRank(scores, your_score):
+	count = 0
+	for score in scores:
+		if your_score >= score:
+			count += 1
+	
+	rank = 100.0 * (count / len(scores))
+	return rank
+	
+def percentile(scores, percentile_rank):
+	scores.sort()
+	for score in scores:
+		if percentileRank(score, scores) >= percentile_rank:
+			return score
